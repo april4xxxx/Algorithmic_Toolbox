6989ffd9bfa45b9bebe480492e84a8f8d90d8d68
april4xxxx <81396543+april4xxxx@users.noreply.github.com>
2025-04-01 15:17:04 +0200

    test using pycharm directly

--- Programming Challenges/Sum of Two Digits/sum_of_two_digits.py
+++ Programming Challenges/Sum of Two Digits/sum_of_two_digits.py
@@ -3,7 +3,7 @@
 
 def sum_of_two_digits(first_digit, second_digit):
     assert 0 <= first_digit <= 9 and 0 <= second_digit <= 9
-    return first_digit + second_digit
+    return first_digit + second_digit # the function should return the sum of two digits
 
 
 if __name__ == '__main__':
