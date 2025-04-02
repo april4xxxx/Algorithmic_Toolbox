a579fbf2c219cdfb694de9cb323d32f5607364d8
april4xxxx <81396543+april4xxxx@users.noreply.github.com>
2025-04-02 15:20:56 +0200

    module 2

--- Algorithmic Warm Up/Last Digit of the Sum of Squares of Fibonacci Numbers/last_digit_of_the_sum_of_squares_of_fibonacci_numbers.py
+++ Algorithmic Warm Up/Last Digit of the Sum of Squares of Fibonacci Numbers/last_digit_of_the_sum_of_squares_of_fibonacci_numbers.py
@@ -19,8 +19,21 @@ def last_digit_of_the_sum_of_squares_of_fibonacci_numbers_naive(n):
 def last_digit_of_the_sum_of_squares_of_fibonacci_numbers(n):
     assert 0 <= n <= 10 ** 18
 
-    type here
+    n_mod = n%60
 
+    fn_mod_10 = fibonacci_mod_10(n_mod)
+    fn_plus_1_mod_10 = fibonacci_mod_10((n_mod+1)%60)
+
+    return (fn_mod_10 * fn_plus_1_mod_10) % 10
+
+def fibonacci_mod_10(n):
+    if n<=1:
+        return n
+
+    prev, curr = 0,1
+    for i in range(2,n+1):
+        prev, curr = curr, (prev+curr)%10
+    return curr
 
 if __name__ == '__main__':
     input_n = int(input())
